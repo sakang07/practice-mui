e27f34e68f0a8f1533622f6846ecea46d6406f65
SA K <sakang07@gmail.com>
2022-07-14 00:24:21 +0900

    [section 2-18] Set default color theme by createTheme and ThemeProvider

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
+import { ThemeProvider } from '@mui/material/styles';
+
+import theme from './ui/Theme';
 import Header from './ui/Header';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
-      {[...new Array(120)]
-        .map(
-          () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-        )
-        .join('\n')}
-    </>
+      Hello
+    </ThemeProvider>
   );
 }
 
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,11 +1,7 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 
 function Header() {
   function ElevationScroll(props) {
@@ -23,7 +19,7 @@ function Header() {
 
   return (
     <ElevationScroll>
-      <AppBar position="fixed">
+      <AppBar position="fixed" color="secondary">
         <Toolbar>Arc Development</Toolbar>
       </AppBar>
     </ElevationScroll>
