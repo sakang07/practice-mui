01113d59203c8efe89ff20ed29ce03b66512acdc
SA K <sakang07@gmail.com>
2022-07-13 23:44:05 +0900

    [section 2-14] AppBar with ElevationScroll

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,5 +1,33 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 function Header() {
-  return <div>Header</div>;
+  function ElevationScroll(props) {
+    const { children } = props;
+
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+    });
+
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
+
+  return (
+    <ElevationScroll>
+      <AppBar position="fixed">
+        <Toolbar>Arc Development</Toolbar>
+      </AppBar>
+    </ElevationScroll>
+  );
 }
 
 export default Header;
